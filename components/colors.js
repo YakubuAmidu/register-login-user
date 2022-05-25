ccf8161977630e81f6df7488c2ac8bdd82b0c3ff
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-05-24 18:45:56 -0700

    Initial commit

---
+++ components/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: '#222831',
+  secondary: '#3939E46',
+  tertiary: '#EEEEEE',
+  accent: '#00ADB5',
+  darkGray: '#111827',
+  white: '#fff',
+  black: '#000',
+  success: '#22C55A',
+  fail: '#EF4444'
+}
