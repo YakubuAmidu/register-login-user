1b3652d468a51eea81c9a3f9aceb098640002f61
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2022-05-31 22:57:26 -0700

    Initial commit

--- components/Cards/InfoCard.js
+++ components/Cards/InfoCard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import RegularText from '../Texts/RegularText';
+import SmallText from '../Texts/SmallText';
 
 // Styled components
 import styled from 'styled-components/native';
@@ -23,10 +25,25 @@ shadow-color: ${ black };
 shadow-offset: 0px 2px;
 shadow-opacity: 0.25;
 shadow-radius: 4px;
-`
+`;
+
+const CardSection = styled.View`
+ justify-content: space-between;
+ align-items: flex-start;
+`;
 
 const InfoCard = (props) => {
-  return <CardView {...props}>{props.children}</CardView>
+  return <CardView {...props}>
+    <CardSection style={{ width: '60%' }}>
+     <RegularText style={{ fontWeight: 'bold'}}>Balance</RegularText>
+     <RegularText style={{ fontWeight: 'bold', fontSize: 25 }}>$ 13,288.65</RegularText>
+     <SmallText>13/05/22</SmallText>
+    </CardSection>
+
+    <CardSection style={{ width: '40%' }}>
+
+    </CardSection>
+  </CardView>
 }
 
 export default InfoCard;
